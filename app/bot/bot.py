09f9a16b1a54570e0566d1d84f89050435fe8e66
hecronk <damiraminov1@yandex.ru>
2022-08-31 02:16:01 +0500

    started writing bot

---
+++ app/bot/bot.py
@@ -0,0 +1,21 @@
+from aiogram import Bot, Dispatcher, executor, types
+
+from config import TelegramBotConfig, ParserConfig
+from app.parser.parser import Parser
+
+bot = Bot(token=TelegramBotConfig.TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands='start')
+async def start(message: types.Message):
+    groups = Parser.get_groups(ParserConfig.URL)
+    markup = types.InlineKeyboardMarkup()
+    for i in range(len(groups)):
+        button = types.InlineKeyboardButton(groups[i], callback_data=groups[i])
+        markup.add(button)
+    await message.reply('Привет! Это бот расписания УрГПУ! Выбери свою группу. Позднее ты сможешь ее изменить в настройках',
+                        reply_markup=markup)
+
+
+# @dp.callback_query_handler
