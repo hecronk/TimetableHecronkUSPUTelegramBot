247350cf470a09b15688c6ac93a8baaa2b99ab6b
hecronk <damiraminov1@yandex.ru>
2022-08-31 01:40:10 +0500

    got data from parsing

--- app/parser.py
+++ app/parser.py
@@ -26,18 +26,45 @@ class Parser:
         return groups.text.split('\n')[2:-2]
 
     @staticmethod
-    def get_content(url: str, group: str):
+    def get_available_days(url: str, group: str):
         try:
             soup = BeautifulSoup(Parser._get_html(url + group).text, 'html.parser')
         except ConnectionError:
             return "Can't Parse!"
         stud_r = soup.find('div', attrs={'class': 'stud-r'})
-        update = stud_r.find('script').get_text().split(r"$('.stud-r .rasp-update').html('")[1][:-8]
-        items = stud_r.find_all('div', attrs={'class': 'rasp-item'})[0]  # paste here day
-        items.find('span', attrs={'class': 'rasp-day'}).getText()
-        items.find('div', attrs={'class': 'rasp-week'}).getText()
-        items.find_all('span', attrs={'class': 'para-time'})[0].getText()  # paste here para time by index
-        return update
+        items = stud_r.find_all('div', attrs={'class': 'rasp-item'})
+        days = list()
+        for item in range(len(items)):
+            days.append({
+                'date': items[item].find('span', attrs={'class': 'rasp-day'}).getText(),
+                'week': items[item].find('div', attrs={'class': 'rasp-week'}).getText()
+            })
+        return days
+
+    @staticmethod
+    def get_content(url: str, group: str, day: dict):
+        try:
+            soup = BeautifulSoup(Parser._get_html(url + group).text, 'html.parser')
+        except ConnectionError:
+            return "Can't Parse!"
+        stud_r = soup.find('div', attrs={'class': 'stud-r'})
+        items = stud_r.find_all('div', attrs={'class': 'rasp-item'})[Parser.get_available_days(url, group).index(day)]  # paste here day
+        lessons_count = len(items.find_all('span', attrs={'class': 'para-time'}))
+        lessons = list()
+        for i in range(lessons_count):
+            data = items.find_all('p')[i].getText().split('\n') # list with lesson, auditorium, teacher, subgroup
+            lessons.append({
+                'time': items.find_all('span', attrs={'class': 'para-time'})[i].getText(),  # paste here para time by index,
+                'lesson_name + auditorium': data[0],
+                'teacher': data[1],
+                'subgroup': data[2]
+            })
+        result = {
+            'update': stud_r.find('script').get_text().split(r"$('.stud-r .rasp-update').html('")[1][:-8],
+            'content': lessons
+        }
+        return result
 
 
-print(Parser.get_content(url=ParserConfig.URL, group='РиА-1931'))
+# print(Parser.get_available_days(url=ParserConfig.URL, group='ХОР-1931oz'))
+print(Parser.get_content(url=ParserConfig.URL, group='ФК-2132', day={'date': '05 сентября', 'week': 'Пн'}))
