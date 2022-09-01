21f1927c8003b5354b72179fe8a9ad2505a3383d
hecronk <damiraminov1@yandex.ru>
2022-09-02 02:08:52 +0500

    deleting unused messages fixes

--- app/bot/bot.py
+++ app/bot/bot.py
@@ -10,7 +10,7 @@ dp = Dispatcher(bot)
 
 
 def get_start_markup() -> types.ReplyKeyboardMarkup:
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
     schedule_button = types.KeyboardButton('Расписание')
     settings_button = types.KeyboardButton('Настройки')
     markup.add(schedule_button, settings_button)
@@ -26,16 +26,18 @@ async def start(message: types.Message):
             button = types.InlineKeyboardButton(groups[i], callback_data=groups[i])
             markup.add(button)
         await message.reply(
-            'Привет! Это бот расписания УрГПУ! Выбери свою группу ↓. Позднее ты сможешь ее изменить в настройках',
+            'Привет! Это бот расписания УрГПУ! Выбери свою группу ↓. Ты всегда сможешь ее изменить в настройках',
             reply_markup=markup)
-        await message.answer(
-            'Привет! Это бот расписания УрГПУ! Выбери свою группу ↑. Позднее ты сможешь ее изменить в настройках')
     else:
-        await message.answer('С вовзращением!', reply_markup=get_start_markup())
+        await message.delete()
+        await message.answer(f'С возвращением!'
+                             f' Ваша группа {str(session.query(User).filter(User.telegram_id == message.from_user.id).first().group)}'
+                             , reply_markup=get_start_markup())
 
 
 @dp.callback_query_handler()
 async def callback_handler(callback: types.CallbackQuery):
+    await callback.answer('Подождите, идет загрузка ...', show_alert=True)
     if callback.message:
         if callback.data in Parser.get_groups(ParserConfig.URL):
             if not session.query(User).filter(User.telegram_id == callback.from_user.id).first():
@@ -45,11 +47,10 @@ async def callback_handler(callback: types.CallbackQuery):
             session.query(User).filter(User.telegram_id == callback.from_user.id).first().group = callback.data
             session.commit()
             group = session.query(User).filter(User.telegram_id == callback.from_user.id).first().group
-            await callback.message.answer('Установлена группа {}.'
-                                          ' Для изменения группы перейдите в настройки'.format(group),
+            await callback.message.answer(f'Установлена группа {group}. Для изменения группы перейдите в настройки',
                                           reply_markup=get_start_markup())
             await callback.message.delete()
-        if callback.data in str(Parser.get_available_days(ParserConfig.URL, group=str(
+        elif callback.data in str(Parser.get_available_days(ParserConfig.URL, group=str(
                 session.query(User).filter(User.telegram_id == callback.from_user.id).first().group))):
             day = ast.literal_eval(callback.data)
             content = Parser.get_content(url=ParserConfig.URL,
@@ -60,14 +61,17 @@ async def callback_handler(callback: types.CallbackQuery):
             for i in range(len(content['content'])):
                 result += 'Время: ' + content['content'][i]['time'] + '\n' + 'Предмет: ' + content['content'][i]['lesson_name + auditorium'] + ('\n' + content['content'][i]['subgroup'].title() if content['content'][i]['subgroup'] != '' else '') + '\n' + content['content'][i]['teacher'] + '\n'*2
             await callback.message.answer(result)
-        if callback.data == '/start':
+            await callback.message.delete()
+        elif callback.data == '/start':
             await start(message=callback.message)
-        if callback.data == '/menu':
-            await callback.message.answer('Добро пожаловать!', reply_markup=get_start_markup())
+            await callback.message.delete()
+        elif callback.data == '/menu':
+            await callback.message.delete()
 
 
 @dp.message_handler(content_types='text')
 async def main(message: types.Message):
+    await message.delete()
     if message.text == 'Расписание':
         group = str(session.query(User).filter(User.telegram_id == message.from_user.id).first().group)
         days = Parser.get_available_days(url=ParserConfig.URL, group=group)
@@ -80,6 +84,6 @@ async def main(message: types.Message):
         markup = types.InlineKeyboardMarkup()
         change_group_button = types.InlineKeyboardButton('Изменить группу', callback_data='/start')
         back_button = types.InlineKeyboardButton('Назад', callback_data='/menu')
-        markup.add(change_group_button, back_button)
-        await message.answer('Меню настроек:', reply_markup=markup)
-
+        markup.add(change_group_button)
+        markup.add(back_button)
+        await message.answer('Настройки:', reply_markup=markup)
